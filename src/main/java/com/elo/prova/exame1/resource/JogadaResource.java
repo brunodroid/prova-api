26d2972966265e5e5bdff310426a2935cdab2ca4
bruno <get.brunonogueira@gmail.com>
2019-06-04 01:55:52 -0300

    heroku @CrossOrigin

--- src/main/java/com/elo/prova/exame1/resource/JogadaResource.java
+++ src/main/java/com/elo/prova/exame1/resource/JogadaResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.websocket.server.PathParam;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/jogos")
 public class JogadaResource {
