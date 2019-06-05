b8e81248f0ff603901e5aac605be105aa07ebf91
bruno <get.brunonogueira@gmail.com>
2019-06-04 23:45:23 -0300

    integração front-end

--- src/main/java/com/elo/prova/ProvaEloApplication.java
+++ src/main/java/com/elo/prova/ProvaEloApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProvaApiApplication {
+public class ProvaEloApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProvaApiApplication.class, args);
+		SpringApplication.run(ProvaEloApplication.class, args);
 	}
 }
--- src/test/java/com/elo/prova/ProvaEloApplicationTests.java
+++ src/test/java/com/elo/prova/ProvaEloApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ProvaApiApplicationTests {
+public class ProvaEloApplicationTests {
 
 	@Test
 	public void contextLoads() {
